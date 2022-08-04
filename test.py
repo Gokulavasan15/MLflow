5cb1b0df32339175e38bc969b850bb01f4f06a42
Gokulavasan S <95906159+Gokulavasan15@users.noreply.github.com>
2022-08-04 11:10:08 +0530

    Update test.py

--- test.py
+++ test.py
@@ -42,7 +42,7 @@ if __name__ == "__main__":
 
     nodes = 10
     with mlflow.start_run():
-        decision_tree_classification = DecisionTreeClassifier(criterion='entropy',max_leaf_nodes =nodes , random_state = 10)
+        decision_tree_classification = DecisionTreeClassifier(criterion='entropy',max_leaf_nodes =params['nodes'] , random_state = 10)
         decision_tree1 = decision_tree_classification.fit(X_train, y_train)
         y_pred=decision_tree1.predict(X_test)
 
