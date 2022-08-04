f95efbb199182d07ce884ad5f29af98fc6899b22
Gokulavasan S <95906159+Gokulavasan15@users.noreply.github.com>
2022-08-04 11:13:45 +0530

    Update test.py

--- test.py
+++ test.py
@@ -41,34 +41,35 @@ if __name__ == "__main__":
     X_train, X_test, y_train, y_test = train_test_split(data, df_target, random_state = 10, test_size = 0.2)
 
     nodes = 10
-    with mlflow.start_run():
-        decision_tree_classification = DecisionTreeClassifier(criterion='entropy',max_leaf_nodes =params['nodes'] , random_state = 10)
-        decision_tree1 = decision_tree_classification.fit(X_train, y_train)
-        y_pred=decision_tree1.predict(X_test)
+    def mlflow_run(params):
+        with mlflow.start_run():
+            decision_tree_classification = DecisionTreeClassifier(criterion='entropy',max_leaf_nodes =params['nodes'] , random_state = 10)
+            decision_tree1 = decision_tree_classification.fit(X_train, y_train)
+            y_pred=decision_tree1.predict(X_test)
 
-        (precision,recall,f1,accuracy) = eval_metrics(y_test,y_pred)
+            (precision,recall,f1,accuracy) = eval_metrics(y_test,y_pred)
 
-        
-        print("  precision: %s" % precision)
-        print("  recall: %s" % recall)
-        print("  f1: %s" % f1)
-        print(" accuracy:%s"% accuracy)
 
-        mlflow.log_param("max_leaf_nodes", nodes)
-        mlflow.log_metric("precision", precision)
-        mlflow.log_metric("recall", recall)
-        mlflow.log_metric("f1", f1)
-        mlflow.log_metric("accuracy", accuracy)
+            print("  precision: %s" % precision)
+            print("  recall: %s" % recall)
+            print("  f1: %s" % f1)
+            print(" accuracy:%s"% accuracy)
 
-        tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+            mlflow.log_param("max_leaf_nodes", nodes)
+            mlflow.log_metric("precision", precision)
+            mlflow.log_metric("recall", recall)
+            mlflow.log_metric("f1", f1)
+            mlflow.log_metric("accuracy", accuracy)
 
-        # Model registry does not work with file store
-        if tracking_url_type_store != "file":
+            tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
 
-            # Register the model
-            # There are other ways to use the Model Registry, which depends on the use case,
-            # please refer to the doc for more information:
-            # https://mlflow.org/docs/latest/model-registry.html#api-workflow
-            mlflow.sklearn.log_model(decision_tree_classification, "model", registered_model_name="DecisionTreeClassifier")
-        else:
-            mlflow.sklearn.log_model(decision_tree_classification, "model")
+            # Model registry does not work with file store
+            if tracking_url_type_store != "file":
+
+                # Register the model
+                # There are other ways to use the Model Registry, which depends on the use case,
+                # please refer to the doc for more information:
+                # https://mlflow.org/docs/latest/model-registry.html#api-workflow
+                mlflow.sklearn.log_model(decision_tree_classification, "model", registered_model_name="DecisionTreeClassifier")
+            else:
+                mlflow.sklearn.log_model(decision_tree_classification, "model")
